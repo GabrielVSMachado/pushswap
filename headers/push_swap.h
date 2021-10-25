c504916558ecd95b63c2fcee99f5ad263f1d7f68
gvitor-s <gvitor-s@student.42sp.org.br>
2021-10-25 18:48:11 -0300

    feat: add functions to convert list of strings in ints

--- headers/push_swap.h
+++ headers/push_swap.h
@@ -12,18 +12,24 @@
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+# define LEN_MIN_INT 11
 
 # include <stdlib.h>
 # include <unistd.h>
+# include <limits.h>
 # include "libft.h"
 
 /* MOVIMENT FUNCTIONS */
-int	swap(t_list **lst);
-int	push(t_list **from, t_list **to);
-int	rotate_to_up(t_list **stack);
-int	rotate_to_down(t_list **stack);
+int		swap(t_list **lst);
+int		push(t_list **from, t_list **to);
+int		rotate_to_up(t_list **stack);
+int		rotate_to_down(t_list **stack);
 
-/* CHECK AND PARSER FUNCTIONS */
-int	check_isdigit(int argc, char **argv);
-int	check_repeated(int argc, char **argv);
+/* FUNCTIONS TO CHECK INPUT */
+int		check_isdigit(int argc, char **argv);
+int		check_repeated(int argc, char **argv);
+int		check_greater_than_max_or_less_than_min_int(int argc, char **argv);
+
+/* FUNCTIONS TO MAKE A LINKED LIST WITH THE INPUTS */
+t_list	*make_llst_of_ints(int size, char **numbers_as_strings);
 #endif

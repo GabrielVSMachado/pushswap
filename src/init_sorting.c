384ff0cd113c8c862b04a1964309e9dab346ec0b
gvitor-s <gvitor-s@student.42sp.org.br>
2021-11-27 20:47:28 -0300

    feat: add check if is sorted and free ord_array, init_sorting

--- src/init_sorting.c
+++ src/init_sorting.c
@@ -21,7 +21,10 @@ void	setup_sorting(int argc, char **argv)
 	stacks.stack_a = make_llst_of_ints(argc, argv);
 	ord_array = make_ints_array(argc, argv);
 	quick_sort(ord_array, 0, argc - 2);
-	if (sorting(&ord_array, &stacks, argc - 1) == ERROR)
-		exit(_error(&stacks));
+	if (!check_sorted(stacks.stack_a))
+		if (sorting(&ord_array, &stacks, argc - 1) == ERROR)
+			exit(_error(&stacks));
+	if (ord_array)
+		free(ord_array);
 	clear_stacks(&stacks);
 }
